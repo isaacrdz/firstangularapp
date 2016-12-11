56a8fc4dfc7350535051e8f7d82fb2315316b036
isaacrdz <zack_cpp@hotmail.com>
2016-12-10 23:08:31 -0600

    almost finished

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,64 @@
-angular.module("todoListApp",[]);
+angular.module("todoListApp",[])
+.controller('mainCtrl', function($scope, dataService){
+
+	$scope.helloConsole  = dataService.helloConsole;
+
+	$scope.addTodo = function(){
+		var todo = {name:"New todo"};
+		$scope.todos.push(todo);
+	};
+	
+	$scope.helloWorld = dataService.helloWorld;
+
+	 dataService.getTodos(function(response){
+				console.log(response.data);
+				$scope.todos =  response.data;
+		});
+
+	 $scope.deleteTodo = function(todo, $index){
+	 	dataService.deleteTodo(todo);
+	 	$scope.todos.splice($index,1);
+	 };
+
+	 $scope.saveTodo = function(todo){
+	 	dataService.saveTodo(todo);
+	 };
+
+})
+.service('dataService', function($http){
+
+	this.helloConsole = function() {
+		console.log('This is the hello console service');
+	};
+
+	this.getTodos = function(callback){
+			$http.get('mock/todos.json')
+			.then(callback)
+	};
+
+	this.deleteTodo = function(todo){
+		console.log("has been deleted");
+
+		//other logic
+	};
+
+	this.saveTodo = function(todo){
+		console.log("The  todo has been saved");
+
+	};
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
